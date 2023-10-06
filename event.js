064534132f6937db27a7fe3baf4af304c160faed
sonali <143820681+Babys4127@users.noreply.github.com>
2023-10-06 21:04:16 +0530

    Add files via upload

---
+++ event.js
@@ -0,0 +1,13 @@
+function showThankYouMessage() {
+    alert("Thank you for your interest in our event!");
+  }
+
+  // Get a reference to the button element
+  var submitButton = document.getElementById("submit-button");
+
+  // Add a click event listener to the button
+  submitButton.addEventListener("click", showThankYouMessage);
+
+
+
+
